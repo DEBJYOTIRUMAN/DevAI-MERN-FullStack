f6ed621042ddf49fc53265f73a3f944b73f0b024
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-02-09 11:28:19 +0530

    Mobile Add Status Bar

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,5 +1,6 @@
 import { WebView } from "react-native-webview";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
         mixedContentMode="compatibility"
         source={{ uri: "https://devai-ruman.vercel.app" }}
       />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
